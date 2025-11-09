60a80c6bb0ba37558d0cf97808e94911f9b9db7f
kozlowskimateusz <mateusz.kozlowski@nextapps.co>
2025-11-09 13:24:55 +0100

    fix/main.ts

--- fe/src/main.ts
+++ fe/src/main.ts
@@ -3,10 +3,39 @@ import "leaflet/dist/leaflet.css";
 
 import { MapCore } from "./MapCore";
 import { eventBus } from "./MapCore/eventBus/EventBus";
+import { MapPlugin } from "./MapCore/Plugins/MapPlugin";
+import { BusPlugin, events } from "./MapCore/Plugins/Buses/BusPlugin";
+import { PeriodicEmitterPlugin } from "./MapCore/Plugins/PeriodicEmitterPlugin";
+
+import { FilterBusesByLineSelected } from "./MapCore/Plugins/Buses/Filters/FilterByLine";
+
+// Create more if needed.
+const PeriodicBusFetcher = PeriodicEmitterPlugin;
 
 const mapCore = new MapCore(eventBus, {
-  plugins: [],
+  plugins: [
+    new MapPlugin(eventBus, {
+      containerId: "map",
+      center: [52.259788, 21.040546],
+      zoom: 13,
+    }),
+    new BusPlugin(eventBus, {
+      path: "/buses",
+      filters: [
+        new FilterBusesByLineSelected(eventBus, {
+          selectElementId: "bus-lines-filter",
+        }),
+      ],
+    }),
+    new PeriodicBusFetcher(eventBus, {
+      eventName: events.busDataFetchNewData,
+      intervalId: 0,
+      fetchInterval: 10000,
+    }),
+  ],
 });
+
 
 // Initialize Plugins
 mapCore.initializePlugins();
+
